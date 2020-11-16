30e8622f5da28625cddec262306e25229740d457
Watcharawit Yuthong <watcharawit.yuthong123@gmail.com>
2020-11-16 22:15:57 +0700

    add stacknavigation

--- App.js
+++ App.js
@@ -3,13 +3,72 @@ import React, { Component } from 'react';
 import {
   View,Text
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { useNavigation } from '@react-navigation/native';
+import { useRoute } from '@react-navigation/native';
+
 import Spalsh from './Spalsh'
 import Login from './Login'
 import Register from './Register'
 import Home from './Home'
 import Song from './Song'
-import ForgotPasssword from './ForgotPassword'
-import Home from './Home'
+import ForgotPassword from './ForgotPassword'
+
+const SpalshScreen =()=> {
+  const navigation = useNavigation();
+  return (
+    <Spalsh navigation={navigation} />
+  );
+}
+
+const LoginScreen =()=> {
+  const navigation = useNavigation();
+  return (
+    <Login navigation={navigation} />
+  );
+}
+
+const RegisterScreen =()=> {
+  const navigation = useNavigation();
+  return (
+    <Register navigation={navigation} />
+  );
+}
+
+const ForgotPasswordScreen =()=> {
+  const navigation = useNavigation();
+  return (
+    <ForgotPassword navigation={navigation} />
+  );
+}
+
+const Stack = createStackNavigator();
+const MyStack=()=>{
+      return (
+        <Stack.Navigator>
+          <Stack.Screen name="Spalsh" 
+            component={SpalshScreen} 
+            options={{ headerShown: true}}/>
+
+          <Stack.Screen name="Login" 
+            component={LoginScreen} 
+            options={{ headerShown: true}}/>
+
+          <Stack.Screen name="Register" 
+            component={RegisterScreen} 
+            options={{ headerShown: true}}/>
+
+          <Stack.Screen name="ForgotPassword" 
+            component={ForgotPasswordScreen} 
+            options={{ headerShown: true}}/>
+
+        </Stack.Navigator>
+      );
+}
+
 export default class App extends Component {
 
   constructor(props){
@@ -21,7 +80,9 @@ export default class App extends Component {
 
   render(props) {
     return (
-      <Home/>
+      <NavigationContainer>
+        <MyStack />
+      </NavigationContainer>
     );
   }
 }
