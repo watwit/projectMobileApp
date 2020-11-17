419bb9071837f9ed234803c9533e12d5b7e2c59c
itthikorn2542 <gorigoe.42@gmail.com>
2020-11-18 01:29:16 +0700

    new commit

--- Food.js
+++ Food.js
@@ -1,26 +1,59 @@
 import React, { Component } from 'react';
 import {
-  View,Text,StyleSheet,Image,TouchableOpacity
+  View,Text,StyleSheet,FlatList,Image,TouchableOpacity
 } from 'react-native';
 import Constants from 'expo-constants';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
-
+import * as meno from './meno.json';
 
 class Food extends Component {
   constructor(props){
     super(props);
-     this.state = {
- 
-    };
+    this.state = {
+      selectedID:null,
+      searchText:null,
+   };
   }
+  renderSeparator=()=>{
+    return(
+      <View style={{height:1,backgroundColor:'black'}}></View>
+    );
+  }
+  renderItem=({item})=>{
+    let backgroundColor = null
+    if(this.state.selectedID==item.id){
+      backgroundColor="#FB7070"
+    }
+    else{
+      backgroundColor="white"
+    }
+    return(
+      <View style={{marginLeft:20}}>
+        <TouchableOpacity >
+        <View style={{height:27,width:'100%',backgroundColor:'white',borderRadius:5}}>
+              <Text style={{margin:5,fontSize:11}}>{item.name}</Text>
+        </View>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
 
   render(props) {
     const { navigation } = this.props;
     return (
-      <View style={{ flex: 1,backgroundColor:"#F1F1F1" ,paddingTop: Constants.statusBarHeight,}}>
-      
-  </View>
+      <View style={{height:50,width:'100%'}}>
+        {/* <Text>kjcflkbcgk</Text> */}
+      <FlatList
+         horizontal
+        data={meno.food}
+        keyExtractor = {item=>item.id}
+        renderItem={this.renderItem}
+        ItemSeparatorComponent={this.renderSeparator}
+        ref={(ref)=>{this.FlatListRef=ref}}
+    />
+    </View>
     );
   }
 }
--- Home.js
+++ Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-  View,Text,StyleSheet,Image,TouchableOpacity
+  View,Text,StyleSheet,FlatList,Image,TouchableOpacity
 } from 'react-native';
 import Constants from 'expo-constants';
 import {createStackNavigator} from '@react-navigation/stack' 
@@ -8,8 +8,8 @@ import {NavigationContainer,useNavigation } from '@react-navigation/native'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import { FontAwesome5 } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
-
-
+import {Card,Avatar,Title,Paragraph} from 'react-native-paper';
+import * as data from './data.json';
 class Home extends Component {
   constructor(props){
     super(props);
@@ -19,12 +19,37 @@ class Home extends Component {
   }
 
 
+  renderItem=({item})=>{
+
+    return(
+      <View style={{padding:8}}>
+        <Card>
+            <Card.Title title={item.name} subtitle="ใส่วันที่" 
+            left={()=>(<Avatar.Image size={50} source={{uri:item.url}}/>)}/>
+            {/* <Card.Content>
+              <Title>{item.name}</Title>
+            </Card.Content> */}
+            <Card.Content>
+            <Paragraph><Text>{item.description}</Text></Paragraph>
+            </Card.Content>
+            {item.type=="img"&&<Card.Cover source={{uri:item.url}}/>}
+          </Card>
+      </View>
+    );
+  };
+
+
   render(props) {
     const { navigation } = this.props;
     return (
-        <View style={{ flex: 1,backgroundColor:"#F1F1F1" ,paddingTop: Constants.statusBarHeight,}}>
-            
-        </View>
+      <View style={{flex:1}}>
+        {/* <Text>kjcflkbcgk</Text> */}
+      <FlatList
+        data={data.test}
+        keyExtractor = {item=>item.id}
+        renderItem={this.renderItem}
+    />
+    </View>
     );
   }
 }
@@ -34,6 +59,13 @@ const styles = StyleSheet.create({
       height:'8%',
       backgroundColor:'black',
       flexDirection:'row',
+    },
+    postBar:{
+      width:"100%",
+      height:"20%",
+      backgroundColor:"white",
+      flexDirection:'row'
+
     }
   });
 
