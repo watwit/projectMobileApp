5720cecadb6deec199bed3cfb6e97b25cb6c21b5
itthikorn2542 <gorigoe.42@gmail.com>
2020-11-22 18:15:15 +0700

    song update

--- Song.js
+++ Song.js
@@ -6,6 +6,7 @@ import Constants from 'expo-constants';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
 import * as song from './song.json'
+
 class Song extends Component {
   constructor(props){
     super(props);
@@ -15,8 +16,8 @@ class Song extends Component {
   }
   renderItem=({item})=>{
     return(
-      <View>
-      <View style={{backgroundColor:'white',height:100,width:'100%',borderWidth:1,borderRadius:10,marginRight:20,flexDirection:'row',marginBottom:15}}>
+      <View style={{margin:10}}>
+      <View style={{backgroundColor:'white',borderWidth:1,borderRadius:10,flexDirection:'row',marginRight:10,marginLeft:10}}>
             <View style={{height:80,width:'15%',borderRadius:10,margin:10,justifyContent:'center',alignItems:'center',flexDirection:'row'}}>
                 <Text style={{color:'black',marginLeft:10,fontSize:20,fontFamily:'kanitSemiBold'}}>{item.id}</Text>
                 <View style={{width:1,height:60,backgroundColor:'black',marginLeft:20}}></View>
@@ -33,11 +34,14 @@ class Song extends Component {
       </View>
     );
   }
+
+  AddSong =()=>{
+    
+  }
   render(props) {
     const { navigation } = this.props;
-    
     return (
-      <View>
+      <View style={{flex:1}}>
         <View style={{backgroundColor:'black',height:100,width:'90%',borderWidth:1,borderRadius:10,marginLeft:20,marginRight:20,flexDirection:'row',marginBottom:15}}>
             <View style={{backgroundColor:'#E2E2E2',height:80,width:80,borderRadius:10,margin:10,justifyContent:'center',alignItems:'center'}}>
                 <Image source={{uri:'https://image.flaticon.com/icons/png/512/49/49831.png'}} style={{height:70,width:70}}></Image>
@@ -52,22 +56,23 @@ class Song extends Component {
         <View style={{backgroundColor:'black',width:200,height:30,borderRadius:5,marginLeft:20,alignItems:'center',justifyContent:'center'}}>
           <Text style={{fontFamily:'kanitSemiBold',color:'white'}}>คิวเพลงทั้งหมด 3 เพลง</Text>
         </View>
-        <View style={{marginTop:20,marginLeft:20,marginRight:20}}>
-            <FlatList
+
+        <FlatList
             data={song.test}
             keyExtractor = {item=>item.id}
             renderItem={this.renderItem}
             ref={(ref)=>{this.FlatListRef=ref}}
-          />
+        />
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={()=>{this.setState({showModal:true})}}
+          style={styles.touchableOpacityStyle}>
+          <View style={styles.floatingButtonStyle}>
+                <Entypo  name="plus" size={24} color="white" />
+          </View>
+        </TouchableOpacity>
 
-
-          <View style={{backgroundColor:'red',height:60,width:'100%',alignItems:'flex-end'}}>
-            <TouchableOpacity onPress={()=>{this.setState({showModal:true})}}>
-              <View style={{backgroundColor:'#FB7070',height:60,width:60,borderRadius:50,alignItems:'center',justifyContent:'center'}}>
-                <Text style={{color:'white',fontSize:40}}>&#43;</Text>
-              </View>
-
-              <Modal 
+        <Modal 
                     transparent={true} 
                     visible={this.state.showModal} 
                     animationType="slide"
@@ -113,9 +118,6 @@ class Song extends Component {
                 </View>
             </View>
           </Modal>
-            </TouchableOpacity>
-          </View>
-        </View>
         
       </View>
     );
@@ -143,7 +145,35 @@ const styles = StyleSheet.create({
 
     elevation: 5,
 
-  }
+  },
+  touchableOpacityStyle: {
+    position: 'absolute',
+    width: 60,
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    right: 30,
+    bottom: 30,
+  },
+  floatingButtonStyle: {
+    resizeMode: 'contain',
+    width: 60,
+    height: 60,
+    backgroundColor:'#FB7070',
+    borderRadius:50,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+
+    elevation: 6,
+    justifyContent:'center',
+    alignItems:'center'
+    //backgroundColor:'black'
+  },
   });
 
 
